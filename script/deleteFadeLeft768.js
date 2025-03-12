2e8be15d4a3e6c10a8b42de97dc8b290bf3cb52e
Ronaldmera <ronaldalme1230@gmail.com>
2025-03-12 14:43:05 -0500

    aos fade en #index

--- script/deleteFadeLeft768.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.innerWidth <= 768) {
-        document.querySelectorAll('[data-aos="fade-left"]').forEach((el) => {
-            el.removeAttribute("data-aos");
-        });
-    }
-});
